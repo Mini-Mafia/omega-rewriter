96fe886127ac15012f67d9d04dfd00fa048c372d
Felix <fschwieb@protonmail.com>
2026-03-11 14:26:57 -0400

    Update rewrite-css.js

--- rewrite-css.js
+++ rewrite-css.js
@@ -1,7 +1,13 @@
 import { rewrite_url } from "./rewrite-urls.js"
 
 function rewrite_css_urls(css) {
-  const regex = /url\(["']?([^"'\s]+)["']?\)/; // Will fix more edge cases later
-  const rewritten = css.replace(regex, (match, g1) => { return `url("${rewrite_url(g1)}")` });
+  let regex = /url\(["']?([^"'\s]+)["']?\)/g; // Will fix more edge cases later
+  let rewritten = css.replace(regex, (match, g1) => { return `url("${rewrite_url(g1)}")` });
+  return rewritten;
+}
+
+function rewrite_simple_imports(css) {
+  let regex = /import\s+["']([^"']+)["']/g;
+  let rewritten = css.replace(regex, (match, g1) => `import("${rewrite_url(g1)}")`);
   return rewritten;
 }
